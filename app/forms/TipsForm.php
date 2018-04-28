baa42be2a102384e01f837aa61c5a2edfe94c4fb
Carlos Sanchez Correa <carlosx-34@hotmail.com>
2018-04-28 16:31:31 -0500

    Validate Tip Form and add Files Inputs

--- app/forms/TipsForm.php
+++ app/forms/TipsForm.php
@@ -5,39 +5,54 @@ use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Hidden;
 use Phalcon\Forms\Element\TextArea;
+use Phalcon\Forms\Element\File;
 
-class LegalsForm extends Form
+class TipsForm extends Form
 {
 
     public function initialize($entity = null, $options = null)
     {
 
-        // In edition the id is hidden
         if (isset($options['edit']) && $options['edit']) {
             $id = new Hidden('id');
-            $nameDisabled = true;
         } else {
             $id = new Text('id');
-            $nameDisabled = false;
         }
 
         $this->add($id);
 
-        $name = new Text('name', [
-            'placeholder' => 'Name',
-            'disabled' => $nameDisabled,
+        $name = new Text('title', [
+            'placeholder' => 'Title',
             'class' => 'form-control',
         ]);
 
         $this->add($name);
 
-        $content = new TextArea('content',[
-            'placeholder' => 'content',
+        $description = new TextArea('description',[
+            'placeholder' => 'description',
             'rows' => '10',
             'class' => 'form-control'
         ]);
 
-        $this->add($content);
+        $this->add($description);
+
+        $date = new Text('date',[
+            'placeholder' => 'date',
+            'class' => 'form-control'
+        ]);
+
+
+        $this->add($date);
+
+        $photo = new File('photo',[]);
+
+        $this->add($photo);
+
+        $video = new File('video',[]);
+
+
+
+        $this->add($video);
 
     }
 }
